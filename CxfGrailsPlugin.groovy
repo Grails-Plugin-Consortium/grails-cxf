b56742afa703ad71ffa7236d1683f6134fd70118
Rube <arubin@vmsinfo.com>
2011-01-07 13:25:44 -0700

    Upgraded to grails 1.3.5 with proper dependency resolution using BuildConfig and upgraded the cxf library as well

--- CxfGrailsPlugin.groovy
+++ CxfGrailsPlugin.groovy
@@ -11,13 +11,13 @@ import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping
 
 class CxfGrailsPlugin {
     // the plugin version
-    def version = "0.6.0"
+    def version = "0.7.0"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.1.1 > *"
+    def grailsVersion = "1.3.5 > *"
     // the other plugins this plugin depends on
     def loadAfter = ['hibernate']
     def observe = ['hibernate']
-    def dependsOn = [hibernate: '1.1 > *']
+    def dependsOn = [hibernate: '1.3.5 > *']
     
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,40 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // uncomment to disable ehcache
+        // excludes 'ehcache'
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    repositories {
+        grailsPlugins()
+        grailsHome()
+        grailsCentral()
+
+        // uncomment the below to enable remote dependency resolution
+        // from public Maven repositories
+        //mavenLocal()
+        mavenCentral()
+        //mavenRepo "http://snapshots.repository.codehaus.org"
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
+    }
+    dependencies {
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+	//compile('org.apache.cxf:cxf-rt-transports-http:2.3.0'){
+	//	excludes 'spring-web'
+	//}
+	compile('org.apache.cxf:cxf-rt-frontend-jaxws:2.3.0'){
+		excludes 'spring-web'
+	}
+	compile('org.apache.cxf:cxf-rt-frontend-jaxrs:2.3.0'){
+		excludes 'xmlbeans', 'spring-web', 'spring-core'
+	}
+        // runtime 'mysql:mysql-connector-java:5.1.5'
+    }
+}
+
