7b7d611bfc29336c99402eeabc2d045c3d2af380
ctoestreich <acetrike@yahoo.com>
2013-01-11 15:53:58 -0600

    fixing annotation for class/interface usage only

--- src/groovy/org/grails/cxf/utils/GrailsCxfEndpoint.groovy
+++ src/groovy/org/grails/cxf/utils/GrailsCxfEndpoint.groovy
@@ -2,10 +2,13 @@ package org.grails.cxf.utils
 
 import java.lang.annotation.RetentionPolicy
 import java.lang.annotation.Retention
+import java.lang.annotation.ElementType
+import java.lang.annotation.Target
 
 /**
  */
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface GrailsCxfEndpoint {
     //address="/v2/#name", name="SimpleOneTwo", expose=[EndpointType.SIMPLE], soap12=true
     String address() default ""
