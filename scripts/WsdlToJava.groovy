fefe99a1a743b6dfdcf2d3ac182d3f57c2d93393
ctoestreich <acetrike@yahoo.com>
2013-04-29 15:27:59 -0500

    Updating script to generate wsdl-2-java files.  Still broken, but making progress

--- scripts/WsdlToJava.groovy
+++ scripts/WsdlToJava.groovy
@@ -16,7 +16,8 @@ target(main: 'Quick way to generate wsdl to java from cxf plugin') {
     depends(checkVersion)
 
     createCli()
-    options = cli.parse(args.tokenize())
+    println 'Using args: ' + doSplit(args)
+    options = cli.parse(doSplit(args))
 
     if(options.help) {
         cli.usage()
@@ -29,6 +30,10 @@ target(main: 'Quick way to generate wsdl to java from cxf plugin') {
 printMessage = { String message -> event('StatusUpdate', [message]) }
 finished = {String message -> event('StatusFinal', [message])}
 errorMessage = { String message -> event('StatusError', [message]) }
+
+private doSplit(string){
+    return string.split(/(\n|[ ]|=)/).collect{ it.trim() }.findResults  { it && it != '' ? it : null }
+}
 
 private wsdl2Java() {
 
@@ -58,25 +63,25 @@ private wsdl2Java() {
             arg(value: '-b')
             arg(value: options.b)
         }
-        if(options?.ca) {
+        if(options?.catalog) {
             arg(value: '-catalog')
-            arg(value: options.ca)
+            arg(value: options.catalog)
         }
         if(options?.d) {
             arg(value: '-d')
             arg(value: options.d)
         }
-        if(options?.cmpl) {
+        if(options?.compile) {
             arg(value: '-compile')
         }
-        if(options?.clsdir) {
+        if(options?.classdir) {
             arg(value: '-classdir')
-            arg(value: options.clsdir)
+            arg(value: options.classdir)
         }
-        if(options?.clnt) {
+        if(options?.client) {
             arg(value: '-client')
         }
-        if(options?.srv) {
+        if(options?.server) {
             arg(value: '-server')
         }
         if(options?.impl) {
@@ -85,10 +90,10 @@ private wsdl2Java() {
         if(options?.all) {
             arg(value: '-all')
         }
-        if(options?.doant) {
+        if(options?.ant) {
             arg(value: '-ant')
         }
-        if(options?.anr) {
+        if(options?.autoNameResolution) {
             arg(value: '-autoNameResolution')
         }
         if(options?.exsh) {
@@ -109,35 +114,35 @@ private wsdl2Java() {
         if(options?.keep) {
             arg(value: '-keep')
         }
-        if(options?.nobind) {
+        if(options?.noAddressBinding) {
             arg(value: '-noAddressBinding')
         }
-        if(options?.exslrpr) {
+        if(options?.exceptionSlurper) {
             arg(value: '-exceptionSlurper')
         }
         if(options?.reserveClass) {
             arg(value: '-reserveClass')
             arg(value: options.reserveClass)
         }
-        if(options?.allow) {
+        if(options?.allowElementReferences) {
             arg(value: '-allowElementReferences')
-            arg(value: options.allow)
+            arg(value: options.allowElementReferences)
         }
-        if(options?.async) {
+        if(options?.asyncMethods) {
             arg(value: '-asyncMethods')
-            arg(value: options.async)
+            arg(value: options.asyncMethods)
         }
-        if(options?.bare) {
+        if(options?.bareMethods) {
             arg(value: '-bareMethods')
-            arg(value: options.bare)
+            arg(value: options.bareMethods)
         }
-        if(options?.mime) {
+        if(options?.mimeMethods) {
             arg(value: '-mimeMethods')
-            arg(value: options.mime)
+            arg(value: options.mimeMethods)
         }
-        if(options?.fault) {
+        if(options?.faultSerialVersionUID) {
             arg(value: '-faultSerialVersionUID')
-            arg(value: options.fault)
+            arg(value: options.faultSerialVersionUID)
         }
         if(options?.mark) {
             arg(value: '-mark-generated')
@@ -153,7 +158,7 @@ private createCli() {
     String usageText = '''
 grails wsdl-to-java --wsdl=<path to wsdl>
     [--package=<package>] [--fe=frontend-name] [--db=databinding-name]
-    [--wv=wsdl--version] [--sn=service-name] [--b=binding-name]
+    [--wv=wsdl-version] [--sn=service-name] [--b=binding-name]
     [--catalog=catalog-file-name] [--d output-directory] [--compile]
     [--classdir=compile-class-dir] [--client] [--server]
     [--impl] [--all] [--ant] [--autoNameResolution] [--exsh=(true/false)]
@@ -188,38 +193,38 @@ See http://cxf.apache.org/docs/wsdl-to-java.html for additional options.'''
     })
 
     cli.with {
-        h longOpt: 'help', 'Prints this help message: See http://cxf.apache.org/docs/wsdl-to-java.html for more details usages of these args.'
-        w longOpt: 'wsdl', args: 1, required: true, 'The path to the wsdl to use.'
-        p longOpt: 'package', args: 1, 'The package to put the generated Java objects in.'
+        help longOpt: 'help', 'Prints this help message: See http://cxf.apache.org/docs/wsdl-to-java.html for more details usages of these args.'
+        wsdl longOpt: 'wsdl', args: 1, required: true, 'The path to the wsdl to use.'
+        'package' longOpt: 'package', args: 1, 'The package to put the generated Java objects in.'
         fe longOpt: 'fe', args: 1, required: false, 'Specifies the frontend. Default is JAXWS. Currently supports only JAXWS frontend and a "jaxws21" frontend to generate JAX-WS 2.1 compliant code. '
         db longOpt: 'db', args: 1, required: false, 'Specifies the databinding. Default is jaxb. Currently supports jaxb, xmlbeans, sdo (sdo-static and sdo-dynamic), and jibx.'
         wv longOpt: 'wv', args: 1, required: false, ''
         sn longOpt: 'sn', args: 1, required: false, ''
         b longOpt: 'b', args: 1, required: false, 'binding name'
-        ca longOpt: 'catalog', args: 1, required: false, ''
+        catalog longOpt: 'catalog', args: 1, required: false, ''
         d longOpt: 'd', args: 1, required: false, ''
-        cmpl longOpt: 'compile', args: 0, required: false, ''
-        clsdir longOpt: 'classdir', args: 1, required: false, ''
-        clnt longOpt: 'client', args: 0, required: false, ''
-        srv longOpt: 'server', args: 0, required: false, ''
+        compile longOpt: 'compile', args: 0, required: false, ''
+        classdir longOpt: 'classdir', args: 1, required: false, ''
+        client longOpt: 'client', args: 0, required: false, ''
+        server longOpt: 'server', args: 0, required: false, ''
         impl longOpt: 'impl', args: 0, required: false, ''
         all longOpt: 'all', args: 0, required: false, ''
-        doant longOpt: 'ant', args: 0, required: false, ''
-        anr longOpt: 'autoNameResolution', args: 0, required: false, ''
+        'ant' longOpt: 'ant', args: 0, required: false, ''
+        autoNameResolution longOpt: 'autoNameResolution', args: 0, required: false, ''
         exsh longOpt: 'exsh', args: 1, required: false, ''
         dns longOpt: 'dns', args: 1, required: false, ''
         dex longOpt: 'dex', args: 1, required: false, ''
         validate longOpt: 'validate', args: 0, required: false, ''
         keep longOpt: 'keep', args: 0, required: false, ''
-        nobind longOpt: 'noAddressBinding', args: 0, required: false, ''
-        exslrpr longOpt: 'exceptionSlurper', args: 0, required: false, ''
+        noAddressBinding longOpt: 'noAddressBinding', args: 0, required: false, ''
+        exceptionSlurper longOpt: 'exceptionSlurper', args: 0, required: false, ''
         reserveClass longOpt: 'reserveClass', args: 1, required: false, ''
-        allow longOpt: 'allowElementReferences', args: 1, required: false, ''
-        async longOpt: 'asyncMethods', args: Option.UNLIMITED_VALUES, valueSeparator: ',' as char, required: false, ''
-        bare longOpt: 'bareMethods', args: Option.UNLIMITED_VALUES, valueSeparator: ',' as char, required: false, ''
-        mime longOpt: 'mimeMethods', args: Option.UNLIMITED_VALUES, valueSeparator: ',' as char, required: false, ''
-        fault longOpt: 'faultSerialVersionUID', args: 1, required: false, ''
-        mark longOpt: 'mark-generated', args: 0, required: false, ''
+        allowElementReferences longOpt: 'allowElementReferences', args: 1, required: false, ''
+        asyncMethods longOpt: 'asyncMethods', args: Option.UNLIMITED_VALUES, valueSeparator: ',' as char, required: false, ''
+        bareMethods longOpt: 'bareMethods', args: Option.UNLIMITED_VALUES, valueSeparator: ',' as char, required: false, ''
+        mimeMethods longOpt: 'mimeMethods', args: Option.UNLIMITED_VALUES, valueSeparator: ',' as char, required: false, ''
+        faultSerialVersionUID longOpt: 'faultSerialVersionUID', args: 1, required: false, ''
+        mark longOpt: 'mark', args: 0, required: false, 'Mark the class as generated'
     }
 }
 
