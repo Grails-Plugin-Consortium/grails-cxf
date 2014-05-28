70c93dcd3ff3f314c46158652e4dd00334378e02
Michael Legart <ml@code3.dk>
2014-05-28 10:56:55 +0200

    Release plugin still conflicts with Geb, was unable to fix be excluding nekohtml etc, so just uncomment again.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -94,9 +94,9 @@ grails.project.dependency.resolution = {
         //remove this before committing.  Only used to release...not test.
         // This still an issue?!?
         // http://grails.1312388.n4.nabble.com/Geb-and-Release-plugin-httpclient-conflicts-td4295238.html
-        build(":release:3.0.1", ':rest-client-builder:1.0.3') {
-            export = false
-        }
+        //build(":release:3.0.1", ':rest-client-builder:1.0.3') {
+            //export = false
+        //}
 
         runtime(":hibernate4:4.3.5.3") {
             export = false
