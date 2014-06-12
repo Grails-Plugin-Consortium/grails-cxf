00e1a59a44a15f958492765e9c7c27ea9d22717e
christian.oestreich <christian.oestreich@bluestembrands.com>
2014-06-12 11:43:56 -0500

    removing breaking plugins for tests, only used for publish

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -92,9 +92,9 @@ grails.project.dependency.resolution = {
         //remove this before committing.  Only used to release...not test.
         // This still an issue?!?
         // http://grails.1312388.n4.nabble.com/Geb-and-Release-plugin-httpclient-conflicts-td4295238.html
-        build(":release:2.2.1", ':rest-client-builder:1.0.3') {
-            export = false
-        }
+//        build(":release:2.2.1", ':rest-client-builder:1.0.3') {
+//            export = false
+//        }
 
         runtime(":hibernate:${grailsVersion}") {
             export = false
