5b1dcd83aa8b8cbb3aea6df3f2a8c85e255efc7a
christian.oestreich <christian.oestreich@bluestembrands.com>
2015-10-28 16:53:47 -0500

    Clear out the grails 2 stuff

--- grails-app/services/org/grails/cxf/test/TruckService.groovy
+++
@@ -1,9 +0,0 @@
-package org.grails.cxf.test
-
-
-//this should NOT get wired up as a service as it is missing the expose static property
-class TruckService {
-    String crushCars() {
-        'CRUNCH'
-    }
-}
