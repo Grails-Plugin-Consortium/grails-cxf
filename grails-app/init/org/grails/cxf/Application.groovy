bf9bec693c11b75c77bbf74835843f44e61e937b
Hans Bogaards <hbogaards@anwb.nl>
2017-03-09 13:06:11 +0100

    Upgraded to Grails 3.1.15 to solve duplicate initialisation

--- grails-app/init/org/grails/cxf/Application.groovy
+++ grails-app/init/org/grails/cxf/Application.groovy
@@ -2,7 +2,9 @@ package org.grails.cxf
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import grails.plugins.metadata.PluginSource
 
+@PluginSource
 class Application extends GrailsAutoConfiguration {
 
 	static void main(String[] args) {
